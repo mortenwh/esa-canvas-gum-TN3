e75e682c70db6185fbe3afe5e377533d7eace30c
Morten W. Hansen <mortenwh@met.no>
2025-11-27 16:05:11 +0100

    Add doppler analyses

---
+++ figures/amazon.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+
+fn = (
+    "https://thredds.met.no/thredds/dodsC/remotesensingenvisat/"
+    "asar-doppler/2010/02/19/"
+    "ASA_WSDH2PRNMI20100219_140227_000597992087_00053_41690_0000.nc")
+
